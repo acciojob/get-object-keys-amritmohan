9f3e8f3a7f91fd6236b93cfcbe4336c3d54f3415
@amritmohan <amritmohan2284@gmail.com>
2025-02-10 01:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+const student = {
+  name: "John",
+};
+
+// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
+}
+
+if (typeof module !== "undefined") {
+  module.exports = getKeys;
 }
